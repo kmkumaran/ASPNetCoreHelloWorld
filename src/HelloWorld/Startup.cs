cc0ecd036b0701d6802ac3b5ea19636d3583d0d0
Steve Lasker <StevenLasker@Hotmail.com>
2016-03-15 19:48:55 -0700

    Baseline with Index.html for load balance health check

---
+++ src/HelloWorld/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using System.Collections;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+
+
+                StringBuilder envVars = new StringBuilder();
+                foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                    envVars.Append((string)de.Key + ":" + (string)de.Value + "---" + Environment.NewLine);
+
+                try
+                {
+                    await context.Response.WriteAsync(envVars.ToString());
+
+                    string machineName = Environment.GetEnvironmentVariable("HOSTNAME");
+                    await context.Response.WriteAsync(machineName);
+
+                }
+                catch (Exception ex)
+                {
+                    await context.Response.WriteAsync(ex.ToString());
+                }
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
